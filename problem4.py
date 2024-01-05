406f70e9c346fe48e19590ce6427b42a764cb254
Fabio Antonio Astore <132024075+FabioXXI@users.noreply.github.com>
2024-01-04 22:53:45 -0300

    Create problem4.py

---
+++ problem4.py
@@ -0,0 +1,26 @@
+def pol(num):
+    sNum = str(num)
+    stack = []
+
+    for i in sNum:
+        stack.append(i)
+    stack.reverse()
+
+    for i in range(len(stack) - 1):
+        if stack[i] != sNum[i]:
+            return False
+    return True
+
+nums = []
+
+for i in range(100, 1000):
+    nums.append(i)
+nums.reverse()
+
+poli = []
+for n1 in nums:
+    for n2 in nums:
+        if pol(n1 * n2):
+            poli.append(n1 * n2)
+
+print(max(poli))
