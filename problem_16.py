9696c99cb2f7a3f72516166379356da09c946e07
FabioXXI <astore.a.fabio@gmail.com>
2024-04-23 17:45:14 -0300

    problem_16

---
+++ problem_16.py
@@ -0,0 +1,15 @@
+def pow(value, expoent):
+    result = value
+    count = 1
+    while count != expoent:
+        result *= 2
+        count += 1
+    return result
+
+numbers = str(pow(2, 1000))
+sum = 0
+
+for i in numbers:
+    sum += int(i)
+
+print(sum)
