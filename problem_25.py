f98838461d76daff5fb4ff09db9f85e95331f82a
FabioXXI <astore.a.fabio@gmail.com>
2024-04-24 10:19:35 -0300

    problem_25

---
+++ problem_25.py
@@ -0,0 +1,16 @@
+data = {1:1, 2:1}
+
+def fibonnaci(value):
+    if value in data:
+        return data[value]
+    data[value] = fibonnaci(value - 1) + fibonnaci(value - 2)
+    return data[value]
+
+count = 1
+while True:
+    if len(str(fibonnaci(count))) != 1000:
+        count += 1
+    else:
+        break
+
+print(count)
