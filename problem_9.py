f3f9a526390328e975a8921b4b2376e80feca766
FabioXXI <astore.a.fabio@gmail.com>
2024-05-06 09:44:48 -0300

    problem 9

---
+++ problem_9.py
@@ -0,0 +1,14 @@
+def find(value):
+    for a in range(value):
+        for b in range(value):
+            if a < b:
+                for c in range(value):
+                    if b < c and pow(a, 2) + pow(b, 2) == pow(c, 2) and a + b + c == value:
+                        return (a, b, c)
+                    continue
+
+product = 1
+for i in find(1000):
+    product *= i
+
+print(product)
