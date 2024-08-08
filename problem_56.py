21d9304554fcd6c767ed6bbbf7c9b28e21fcc875
ninja <astore.a.fabio@gmail.com>
2024-08-08 13:33:03 -0300

    problem_56

---
+++ problem_56.py
@@ -0,0 +1,14 @@
+from typing import Generator
+
+def get_pow_and_get_digit_sum() -> Generator:
+    for a in range(1, 100):
+        for b in range(1, 100):
+            yield get_sum(str(a ** b))
+
+def get_sum(n: str):
+    total = 0
+    for i in n:
+        total += int(i)
+    return total
+
+print(max(get_pow_and_get_digit_sum()))
