da75d9d4aee6d549670b4296675c94505d86bc21
ninja <astore.a.fabio@gmail.com>
2024-08-08 13:21:48 -0300

    problem_34

---
+++ problem_34.py
@@ -0,0 +1,20 @@
+from operator import mul
+from functools import reduce
+
+def factorial(n: int) -> int:
+    return reduce(mul, range(1, n + 1), 1)
+
+def get_digit_factorial(n: str) -> int:
+    total = 0
+    for c in n:
+        total += factorial(int(c))
+    return total
+
+def is_equal_fac_digit(n: int) -> bool:
+    return n == get_digit_factorial(str(n))
+
+def get_numbers(n):
+    for i in range(3, n + 1):
+        if is_equal_fac_digit(i): yield i
+
+print(sum(get_numbers(10 ** 5)))
