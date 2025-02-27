7bc2c91c1f727c1d7d91350c30a83fe14d8953f3
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2025-02-27 16:21:31 -0300

    problem_13.py

---
+++ problem_13.py
@@ -0,0 +1,9 @@
+FILE_NAME = "problem_13.txt"
+def get_numbers() -> list[int]:
+    numbers = []
+    with open(FILE_NAME, "r") as file:
+        for n in file.readlines():
+            numbers.append(int(n))
+    return numbers
+
+print(str(sum(get_numbers()))[0:10:])
