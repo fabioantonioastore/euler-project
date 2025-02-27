20bdd3fb83d7929748050dd5c20b4c7a481168c2
Fabio Antonio Astore <astore.a.fabio@gmail.com>
2025-02-27 17:01:51 -0300

    problem_11

---
+++ problem_11.py
@@ -0,0 +1,46 @@
+from array import array
+
+
+FILE_NAME = "problem_11.txt"
+def get_matrix() -> list[array]:
+    matrix = []
+    with open(FILE_NAME, "r") as file:
+        for line in file.readlines():
+            row = array("I")
+            for number in line.split(" "):
+                row.append(int(number))
+            matrix.append(row)
+    return matrix
+
+matrix = get_matrix()
+
+max_product = 0
+
+for row in range(len(matrix)):
+    for collumn in range(len(matrix[0])):
+        p_down = 1
+        p_right = 1
+        p_diagonal = 1
+        p_rev_diagonal = 1
+        for i in range(4):
+            try:
+                p_down *= matrix[row + i][collumn]
+            except:
+                pass
+            try:
+                p_right *= matrix[row][collumn + i]
+            except:
+                pass
+            try:
+                p_diagonal *= matrix[row + i][collumn + i]
+            except:
+                pass
+            try:
+                p_rev_diagonal *= matrix[row - i][collumn + i]
+            except:
+                pass
+        max_prod = max(p_diagonal, p_down, p_right, p_rev_diagonal)
+        if max_prod > max_product:
+            max_product = max_prod
+
+print(max_product)
