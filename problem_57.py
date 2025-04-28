16799e69f2dd980241d5a1b076d73ee792b5b03d
ninja <astore.a.fabio@gmail.com>
2025-04-28 16:15:22 -0300

    refactor

--- problem_57.py
+++ problem_57.py
@@ -6,65 +6,6 @@ from sys import setrecursionlimit
 setrecursionlimit(10 ** 6)
 
 
-class Node:
-    def __init__(self, value: Any, next: 'Node' = None) -> None:
-        self.value = value
-        self.next = next
-
-    def __repr__(self) -> str:
-        return f"Node({self.value!r}, {self.next!r})"
-
-
-class Stack:
-    def __init__(self) -> None:
-        self.size = 0
-        self.top = None
-        self.__iter_stack = None
-
-    def append(self, item: Any) -> None:
-        node = Node(item)
-        self.size += 1
-        if self.top is None:
-            self.top = node
-            return
-        node.next = self.top
-        self.top = node
-
-    def pop(self) -> Any:
-        self.size -= 1
-        node = self.top
-        self.top = node.next
-        return node.value
-
-    def __len__(self) -> int:
-        return self.size
-
-    def __list__(self) -> list[Any]:
-        node = self.top
-        l = []
-        while not node is None:
-            l.append(node.value)
-            node = node.next
-        return l
-
-    def __iter__(self) -> 'Stack':
-        if not self.__iter_stack:
-            self.__iter_stack = Stack()
-        self.__iter_stack.append(self.top)
-        return self
-
-    def __next__(self) -> Any:
-        node = self.__iter_stack.top.value
-        if node is None:
-            self.__iter_stack.pop()
-            raise StopIteration
-        self.__iter_stack.top.value = node.next
-        return node.value
-
-    def __repr__(self) -> str:
-        return f"Stack({list(self)!r})"
-
-
 class Fraction:
     def __init__(self, numerator: int, denominator: int) -> None:
         self.numerator = numerator
